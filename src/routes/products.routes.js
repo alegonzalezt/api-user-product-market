cb9e3ddd5cef1bde3986affc58f588a091c73bc0
alegonzalezt <gonzalezalejandro1010@hotmail.com>
2021-05-27 19:25:34 -0500

    cambios de funciones

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -2,28 +2,18 @@ import { Router } from "express";
 const router = Router();
 
 import * as productsCtrl from "../controllers/products.controller";
-import { authJwt } from "../middlewares";
+//import { authJwt } from "../middlewares";//
+
+//para confirmar accion con token [authJwt.verifyToken, authJwt.isModerator],//
 
 router.get("/", productsCtrl.getProducts);
 
 router.get("/:productId", productsCtrl.getProductById);
 
-router.post(
-  "/",
-  [authJwt.verifyToken, authJwt.isModerator],
-  productsCtrl.createProduct
-);
+router.post("/", productsCtrl.createProduct);
 
-router.put(
-  "/:productId",
-  [authJwt.verifyToken, authJwt.isModerator],
-  productsCtrl.updateProductById
-);
+router.put("/:productId", productsCtrl.updateProductById);
 
-router.delete(
-  "/:productId",
-  [authJwt.verifyToken, authJwt.isAdmin],
-  productsCtrl.deleteProductById
-);
+router.delete("/:productId",productsCtrl.deleteProductById);
 
 export default router;
