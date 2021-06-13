1f0274bccc01e0750fb28ce67e92dfb88d3cb5bf
alegonzalezt <gonzalezalejandro1010@hotmail.com>
2021-06-13 12:58:21 -0500

    subir

--- src/config.js
+++ src/config.js
@@ -3,6 +3,5 @@ config();
 
 export default {
   MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://api-marketplus:M86mfbjQWzqRpwie@cluster0.wjn3f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  PORT: process.env.PORT || 4000,
-  SECRET: 'products-api'
+  PORT: process.env.PORT || 4000
 };
