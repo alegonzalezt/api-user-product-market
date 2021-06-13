01ea31fca9087d454f8e32e22cceb3c54815525d
alegonzalezt <gonzalezalejandro1010@hotmail.com>
2021-06-13 13:03:49 -0500

    add

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ import { config } from "dotenv";
 config();
 
 export default {
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://api-marketplus:M86mfbjQWzqRpwie@cluster0.wjn3f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://api-market-plus:colombia1998@api-market-plu.uz1qa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   PORT: process.env.PORT || 4000
 };
