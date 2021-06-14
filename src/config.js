a7cc782d70f8705b1275646d2effd41814674298
alegonzalezt <gonzalezalejandro1010@hotmail.com>
2021-06-13 22:12:44 -0500

    secret

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ config();
 
 export default {
   MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://api-market-plus:colombia1998@api-market-plu.uz1qa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  PORT: process.env.PORT || 4000
+  PORT: process.env.PORT || 4000,
+  SECRET: 'api-market-plus'
 };
